35ea45fa89eba91adacaa04f65a08ea5626003fe
zach7815 <53745970+zach7815@users.noreply.github.com>
2022-02-18 11:31:53 +0000

    refactored the code to improve readibility.

--- index.js
+++ index.js
@@ -4,87 +4,93 @@ const defaultNote =document.querySelector("#defaultNote");
 const defaultDate=document.querySelector("#defaultDate"); 
 const LocalDate= new Date().toLocaleDateString();
 const LocalTime = new Date().toLocaleTimeString();
-defaultDate.innerHTML= `${LocalDate} ${LocalTime}`;
-const alldeleteButtons= document.querySelectorAll(".exitButton")
+const dateStamp= `${LocalDate} ${LocalTime}`;
+defaultDate.innerHTML= dateStamp;
+const deleteBtns= document.querySelectorAll(".exitButton");
 
 
+
+
+let userInput ={
+    formTitleContent: document.querySelector(".title"),
+    formNoteContent: document.querySelector(".note")
+
+};
 
 
 function createNote(){
-
-    let noteForm= document.querySelector("#noteForm");
-    let formTitleContent = document.querySelector(".title").value;
-    let formNoteContent = document.querySelector(".note").value;
-    const note=document.createElement("div");
-    const noteTitle=document.createElement("h4");
-    const noteContent =document.createElement("p");
-    const noteDate =document.createElement("span");
-    const deleteNoteBtn = document.createElement("span");
-    deleteNoteBtn.innerHTML="x";
-    deleteNoteBtn.classList.add("exitButton")
-
-
     if(notesContainer.lastElementChild===defaultNote){
-    
-        if(formTitleContent=== ""|| formNoteContent===""){
-            alert("Please give your note a title and some content");
 
+        if(userInput.formTitleContent.value=== ""|| userInput.formNoteContent.value=== ""){
+        alert("please enter an note title and note content.")
         }
         else{
-            note.classList.add("renderedNote");
-            noteTitle.innerHTML=formTitleContent;
-            noteDate.classList.add("date");
-            noteDate.innerHTML= `${LocalDate} ${LocalTime}`;
-            noteContent.innerHTML=formNoteContent;
-            note.appendChild(noteTitle);
-            note.appendChild(noteDate);
-            note.appendChild(deleteNoteBtn)
-            note.appendChild(noteContent)
-            notesContainer.replaceChild(note,defaultNote);
-            noteForm.reset();
+            defaultNote.remove()
+        renderElements()
         }
-
-        }
-
-
+    }
     
     else{
-
-        if(formTitleContent=== ""|| formNoteContent===""){
-            alert("Please give your note a title and some content");
-
+        if(userInput.formTitleContent.value=== ""|| userInput.formNoteContent.value===""){
+            alert("please enter an note title and note content.")
         }
+        else{
+            renderElements()
+        };
+    };
+};
 
-else{
-            note.classList.add("renderedNote");
-            noteTitle.innerHTML=formTitleContent;
-            noteDate.classList.add("date");
-            noteDate.innerHTML= `${LocalDate} ${LocalTime}`;
-            noteContent.innerHTML=formNoteContent;
+
+
+
+
+
+function renderElements(){
+    const note = document.createElement("div");
+    const  noteTitle = document.createElement("h4");
+       const noteContent= document.createElement("p");
+     const  noteDate=document.createElement("span");
+     const  deleteNoteBtn=document.createElement("span");
+
+
+     function addClassesAndContent(note,noteTitle,noteContent,noteDate,deleteNoteBtn){
+        note.classList.add("renderedNote")
+        deleteNoteBtn.classList.add("exitButton");
+        noteDate.classList.add("date")
+        deleteNoteBtn.innerHTML="x";
+        noteTitle.innerHTML=userInput.formTitleContent.value;
+        noteContent.innerHTML=userInput.formNoteContent.value;
+        noteDate.innerHTML=dateStamp;
+        }
+        function appendNote(note,noteTitle,noteContent,noteDate,deleteNoteBtn){
             note.appendChild(noteTitle);
             note.appendChild(noteDate);
-            note.appendChild(deleteNoteBtn)
-            note.appendChild(noteContent)
-        notesContainer.appendChild(note);
-        noteForm.reset();
+            note.appendChild(deleteNoteBtn);
+            note.appendChild(noteContent);
+            notesContainer.appendChild(note);
+            noteForm.reset();
+            deleteBtns.forEach(btn=>{
+                btn.addEventListener("click", ()=> {
+                    btn.parentNode.parentNode.removeChild(btn.parentNode)
+                })
+            })
+        }
 
+        addClassesAndContent(note,noteTitle,noteContent,noteDate,deleteNoteBtn)
+        appendNote(note,noteTitle,noteContent,noteDate,deleteNoteBtn)
 }
-       
-    
-    }
- 
-}
-
-function deleteNote(element){
-   element.parentElement.remove()
-}
 
 
-alldeleteButtons.forEach(x=>x.addEventListener("click",()=>{
-    deleteNote(note)
-}))
+
+
+
+
+
+
+
+
 
 btn.addEventListener("click", (event)=>{
     event.preventDefault();
     createNote();
-} )
+} );
